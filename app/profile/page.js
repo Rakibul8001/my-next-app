36821f0844ef2475b4f8ea012900adc18f9e5e8d
Rakibul8001 <ghumpori8001@gmail.com>
2023-11-16 00:14:40 +0600

    .

--- app/profile/page.js
+++ app/profile/page.js
@@ -19,7 +19,7 @@ const page = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link><Link href="/">Home</Link></Nav.Link>
               <Nav.Link href="#link">Link</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
